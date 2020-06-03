e3eed4a8df67524c495f36f6f531fe7d68943c37
haileyli <hailey@Haileys-MacBook-Air-2.local>
2020-06-02 22:07:25 -0400

    Updates

---
+++ precache-manifest.3a74cf2a3633572a9e32d652305cc8ef.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dce3fd7ed430e9039b4",
+    "url": "/Air-QuesBank/static/css/main.ae8d1993.chunk.css"
+  },
+  {
+    "revision": "4dce3fd7ed430e9039b4",
+    "url": "/Air-QuesBank/static/js/main.598ee502.chunk.js"
+  },
+  {
+    "revision": "6f7c0f0de0b5360a936f",
+    "url": "/Air-QuesBank/static/js/runtime~main.53d4e9e8.js"
+  },
+  {
+    "revision": "2f7a735360f2dff7063a",
+    "url": "/Air-QuesBank/static/css/2.8fcd46d2.chunk.css"
+  },
+  {
+    "revision": "2f7a735360f2dff7063a",
+    "url": "/Air-QuesBank/static/js/2.d8315d84.chunk.js"
+  },
+  {
+    "revision": "e6fd3098d9ac5da4c1c05bd5f704b04a",
+    "url": "/Air-QuesBank/static/media/home-bg.e6fd3098.jpeg"
+  },
+  {
+    "revision": "fda365388a189a10009cb7984c69f6da",
+    "url": "/Air-QuesBank/static/media/cloud-bg.fda36538.jpg"
+  },
+  {
+    "revision": "223a51b0e892da02017bc1040f0656fa",
+    "url": "/Air-QuesBank/static/media/air-eng.223a51b0.jpg"
+  },
+  {
+    "revision": "52d302cb9ea04a48bf2754bf4149a9b6",
+    "url": "/Air-QuesBank/static/media/quesbank.52d302cb.jpeg"
+  },
+  {
+    "revision": "0b06a3217e02e1b9f1b625e809485b89",
+    "url": "/Air-QuesBank/static/media/paper.0b06a321.jpeg"
+  },
+  {
+    "revision": "eaf802b605b31e2ec80599680f74bb38",
+    "url": "/Air-QuesBank/static/media/management.eaf802b6.jpeg"
+  },
+  {
+    "revision": "ba986aa7da6402b52f1c2879b2909ea5",
+    "url": "/Air-QuesBank/static/media/login.ba986aa7.jpeg"
+  },
+  {
+    "revision": "034f21cb4ef56fad9498656ffd7f16a3",
+    "url": "/Air-QuesBank/index.html"
+  }
+];
